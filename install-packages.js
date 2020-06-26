5485186a79aa216fe1af4a497271b49b3f7873ac
Nicolas Thouvenin <nthouvenin@gmail.com>
2020-06-26 23:13:58 +0200

    useless

--- install-packages.js
+++
@@ -1,7 +0,0 @@
-const { packages } = require('./config.json');
-const npm = require('npm-programmatic');
-
-console.log("Installing packages from config.json...");
-npm.install(packages || [], { output: true,  cwd: __dirname })
-    .then(() => console.log("All packages installed."))
-    .catch((e) => console.error('Unable to install package', e));
