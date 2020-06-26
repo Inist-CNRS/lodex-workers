1cab5938c0dcd1cb023af258a8d80cb962c1ff21
Nicolas Thouvenin <nthouvenin@gmail.com>
2020-06-26 23:12:01 +0200

    spaces

--- generate-dotenv.js
+++ generate-dotenv.js
@@ -5,6 +5,6 @@ const vars = Object.keys(environnement)
     .filter(key => typeof environnement[key] !== 'object')
     .map(key => `${key}=${JSON.stringify(environnement[key])}`)
     .join('\n');
-process.stdout.write('Writing .env file...');
+process.stdout.write('Writing .env file... ');
 fs.writeFileSync(path.join(__dirname,'.env'), vars);
 process.stdout.write('done.\n');
