1a8ddcc21fbab3d953c00da0adec8b684b930489
Nicolas Thouvenin <nthouvenin@gmail.com>
2021-03-04 15:34:13 +0100

    fix package install bug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p /sbin/.npm /sbin/.config
 chown -R daemon:daemon /app /tmp /sbin/.npm /sbin/.config
-PACKAGES=`node -e 'process.stdout.write(Array(require("./config.json").packages).join(" ").trim())'`
+PACKAGES=`node -e 'process.stdout.write(Array().concat(require("./config.json").packages).filter(Boolean).join(" ").trim())'`
 [ -n "${PACKAGES}" ] && npm install --production  ${PACKAGES} &
 exec su-exec daemon:daemon $*
