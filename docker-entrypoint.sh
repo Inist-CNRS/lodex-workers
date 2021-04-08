c32c6f170d7d93f1c385ac3a17e1ec6fd68844bd
Nicolas Thouvenin <nthouvenin@gmail.com>
2021-04-08 15:48:36 +0200

    add watcher

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,5 @@ mkdir -p /sbin/.npm /sbin/.config
 chown -R daemon:daemon /app /tmp /sbin/.npm /sbin/.config
 PACKAGES=`node -e 'process.stdout.write(Array().concat(require("./config.json").packages).filter(Boolean).join(" ").trim())'`
 [ -n "${PACKAGES}" ] && npm install --production  ${PACKAGES} &
+npm run watcher &
 exec su-exec daemon:daemon $*
